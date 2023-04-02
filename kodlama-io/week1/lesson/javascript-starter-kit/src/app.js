fd68fdb6ced30894f3c9c38a4c613b6c9b57fe51
Coders club <1yavuzahmet@gmail.com>
2023-04-02 03:35:04 +0300

    second

--- kodlama-io/week1/lesson/javascript-starter-kit/src/app.js
+++ kodlama-io/week1/lesson/javascript-starter-kit/src/app.js
@@ -21,5 +21,5 @@ let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi",[
 console.log(konutKredileri)
 
 for (let i=0;i<konutKredileri.length;i++){
-    console.log(konutKredileri[i])
+    console.log(konutKredileri[i]);
 }
