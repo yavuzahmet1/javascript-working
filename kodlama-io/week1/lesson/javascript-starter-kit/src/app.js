6b4495f9fd45669b55737a667a2c16aafac149ad
Coders club <1yavuzahmet@gmail.com>
2023-04-02 03:23:31 +0300

    javascript->array lesson

--- kodlama-io/week1/lesson/javascript-starter-kit/src/app.js
+++ kodlama-io/week1/lesson/javascript-starter-kit/src/app.js
@@ -1,1 +1,25 @@
 console.log("merhaba Kodlama.io")
+
+let dolarDun=9.20
+let dolarBugun=9.44
+{
+    let dolarDun=8.99
+    //var dolarDun=8.99 olsaydı global değişken olacğından hata alacaktık
+    
+}
+console.log(dolarDun)//süslü parantez içindekini çalıştırmaz çünkü orası ayrı blok olarak tanımlanır
+console.log(dolarBugun1)
+let dolarBugun1=9
+
+const euroDun=11
+
+console.log(euroDun)
+
+//array
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi",["a","b","c"]]
+
+console.log(konutKredileri)
+
+for (let i=0;i<konutKredileri.length;i++){
+    console.log(konutKredileri[i])
+}
