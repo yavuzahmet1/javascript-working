86047497c156cefef24876d8829a987d868dae64
Coders club <1yavuzahmet@gmail.com>
2023-04-03 13:12:56 +0300

    javascript->for week 1 homework let keyword

--- kodlama-io/week1/hw/jscamp-master/src/app.js
+++ kodlama-io/week1/hw/jscamp-master/src/app.js
@@ -1,5 +1,11 @@
-let sayi1=10;
+let sayi1 = 10;
 
-sayi1="Ahmet";
+sayi1 = "Ahmet";
+let student = { id: 1, name: "Ahmet", job: "student" }//object notation denir
+//console.log(student);
 
-console.log(sayi1);
+function save(ogrenci,puan=10) {
+    console.log(ogrenci.name+" : "+puan);
+}
+
+save(student,44);//puanı göndermezsek default değeri yani 10'u alır gönderirsek 44 olur
