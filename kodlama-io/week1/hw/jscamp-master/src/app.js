bd733d204f67475e33d72ce1eee2e3447241de82
Coders club <1yavuzahmet@gmail.com>
2023-04-03 14:33:20 +0300

    javascript->for week 1 homework destructuring

--- kodlama-io/week1/hw/jscamp-master/src/app.js
+++ kodlama-io/week1/hw/jscamp-master/src/app.js
@@ -5,7 +5,43 @@ let student = { id: 1, name: "Ahmet", job: "student" }//object notation denir
 //console.log(student);
 
 function save(ogrenci,puan=10) {
-    console.log(ogrenci.name+" : "+puan);
+    //console.log(ogrenci.name+" : "+puan);
 }
 
 save(student,44);//puanı göndermezsek default değeri yani 10'u alır gönderirsek 44 olur
+
+let students=["Ahmet","Yavuz","Ali"];
+
+//console.log(students)
+
+let students1=[students,{id:2,name:"Halit"},"Ankara",{city:"Adana"}]
+
+//console.log(students1)
+
+//REST
+
+let showProducts=function(id,...products){
+    console.log(id)
+    console.log(products)
+}
+
+//console.log(typeof showProducts)//en son aldığı türü öğrenebiliriz,
+
+//showProducts(10,"Karpuz","Armut","Elma")
+
+//SPREAD-> ayrıştırmak
+
+let points=[1,33,23,2,5,4,99]
+//console.log(...points)
+//console.log(Math.max(...points))
+//console.log(Math.max(points))
+
+//DESTRUCTURING
+let population=[1000,2000,3000,[999,6777]]
+let [small,medium,high,[veryHigh,veryMax]]=population
+
+console.log(small)
+console.log(medium)
+console.log(high)
+console.log(veryHigh)
+console.log(veryMax)
