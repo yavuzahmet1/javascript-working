11e26694452977597be6855873920cc3ba1fcb71
Coders club <1yavuzahmet@gmail.com>
2023-04-04 13:30:39 +0300

    javascript->for week 2 referans type

--- kodlama-io/week2/lesson/javascript-starter-kit/src/functions.js
+++ kodlama-io/week2/lesson/javascript-starter-kit/src/functions.js
@@ -18,3 +18,17 @@ let sayHello2=function(params){
 }
 
 sayHello2()
+
+let product1={productName:"Ananas",unitPrice:100,quantity:21}
+
+function addToCart1(product){
+    console.log("Ürün : "+product.productName+" Adet : "+product.quantity+" Fiyat : "+product.unitPrice)
+}
+
+addToCart1(product1)
+
+let product2={productName:"Muz",unitPrice:100,quantity:21}
+let product3={productName:"Karpuz",unitPrice:107,quantity:6}
+product2=product3
+product2.productName="ERIK"
+console.log(product3.productName)
