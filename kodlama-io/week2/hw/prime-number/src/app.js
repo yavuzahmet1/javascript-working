fe5015ca8d270b40a9fe50ba3087f56950ce386d
Coders club <1yavuzahmet@gmail.com>
2023-04-05 14:32:53 +0300

    javascript->for week 2 homework working

---
+++ kodlama-io/week2/hw/prime-number/src/app.js
@@ -0,0 +1,24 @@
+console.log("deneme")
+
+function findPrime(...params) {
+    //let counter
+    for (let index = 0; index < params; index++) {
+        
+        console.log(index)
+    }
+}
+
+function add(...numbers) {
+
+    console.log(numbers[2])
+  /*  let total = 0;
+    for (let index = 0; index < numbers.length; index++) {
+
+        total += numbers[index]
+
+    }
+    console.log(total)*/
+}
+
+add(23,7,8)
+//findPrime(2,5,8,21,13)
