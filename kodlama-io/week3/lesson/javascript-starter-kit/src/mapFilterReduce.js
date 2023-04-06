775c65bfbdf0c29aeabbfa634d1eddf772df2644
Coders club <1yavuzahmet@gmail.com>
2023-04-06 14:27:52 +0300

    javascript->for week 3 filter method

--- kodlama-io/week3/lesson/javascript-starter-kit/src/mapFilterReduce.js
+++ kodlama-io/week3/lesson/javascript-starter-kit/src/mapFilterReduce.js
@@ -1,36 +1,43 @@
-let cart=[
-    {id:1,productName:"Telephone",quantity:3,unitPrice:4000},
-    {id:2,productName:"Cam",quantity:7,unitPrice:88000},
-    {id:3,productName:"Beyaz Eşya",quantity:5,unitPrice:90000},
-    {id:4,productName:"Ev Aleti",quantity:2,unitPrice:4443},
-    {id:4,productName:"Aksesuar",quantity:9,unitPrice:6510}
+let cart = [
+    { id: 1, productName: "Telephone", quantity: 3, unitPrice: 4000 },
+    { id: 2, productName: "Cam", quantity: 7, unitPrice: 88000 },
+    { id: 3, productName: "Beyaz Eşya", quantity: 5, unitPrice: 90000 },
+    { id: 4, productName: "Ev Aleti", quantity: 2, unitPrice: 4443 },
+    { id: 4, productName: "Aksesuar", quantity: 9, unitPrice: 6510 }
 ]
 
 function addToCart(cart) {
-    cart.push({id:7,productName:"Monitör",quantity:2,unitPrice:150})
+    cart.push({ id: 7, productName: "Monitör", quantity: 2, unitPrice: 150 })
 }
 
 addToCart(cart)
 
 console.log(cart)
+//.map 
+cart.map(product => console.log(product.productName));
 
-cart.map(product=>console.log(product.productName));
+console.log("<ul>")
+cart.map(product => {
+    console.log(
+        "<li>"
+        + product.productName + " : "
+        + product.unitPrice * product.quantity
+        + "<li>")})
 
-cart.map(product =>{
-    console.log("<li>"+product.productName+" : "+product.unitPrice*product.quantity+"<li>")
-})  
+console.log("<ul>")
+
+//.filter
+let quantityOver5=cart.filter(product=>product.quantity>5&&product.unitPrice>5000)
+console.log(quantityOver5)
 
 
 
-
-
-
-let number=10
+/*let number = 10
 
 function totalNumber(num) {
-    num+=1
+    num += 1
     console.log(num)
 }
 totalNumber(number)
 console.log(number)
-console.log(totalNumber(number))
+console.log(totalNumber(number))*/
