f3f4bb118fe4da913c529db6a7a4cb251949d5f1
Coders club <1yavuzahmet@gmail.com>
2023-04-06 14:14:56 +0300

    javascript->for week 3 map method

--- kodlama-io/week3/lesson/javascript-starter-kit/src/mapFilterReduce.js
+++ kodlama-io/week3/lesson/javascript-starter-kit/src/mapFilterReduce.js
@@ -5,6 +5,32 @@ let cart=[
     {id:4,productName:"Ev Aleti",quantity:2,unitPrice:4443},
     {id:4,productName:"Aksesuar",quantity:9,unitPrice:6510}
 ]
+
+function addToCart(cart) {
+    cart.push({id:7,productName:"Monitör",quantity:2,unitPrice:150})
+}
+
+addToCart(cart)
+
 console.log(cart)
-cart.push({id:7,productName:"Monitör",quantity:2,unitPrice:150})
-console.log(cart)
+
+cart.map(product=>console.log(product.productName));
+
+cart.map(product =>{
+    console.log("<li>"+product.productName+" : "+product.unitPrice*product.quantity+"<li>")
+})  
+
+
+
+
+
+
+let number=10
+
+function totalNumber(num) {
+    num+=1
+    console.log(num)
+}
+totalNumber(number)
+console.log(number)
+console.log(totalNumber(number))
