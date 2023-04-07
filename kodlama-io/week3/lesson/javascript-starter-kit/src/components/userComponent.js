9efb1ddca01f27cc0e9063763a9425b5a944f8ce
Coders club <1yavuzahmet@gmail.com>
2023-04-07 12:24:52 +0300

    logger-userService-crossCutting

--- kodlama-io/week3/lesson/javascript-starter-kit/src/components/userComponent.js
+++ kodlama-io/week3/lesson/javascript-starter-kit/src/components/userComponent.js
@@ -1,9 +1,10 @@
 import UserService from "../services/userService.js"
 import User from "../models/user.js"
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConserns/logging/logger.js"
 
-console.log("User component loaded !")
-
-let userService = new UserService()
+//console.log("User component loaded !")
+let logger1=new MongoLogger()
+let userService = new UserService(logger1)
 
 userService.add()
 userService.getById(3)
@@ -16,5 +17,9 @@ customer.lastName="YAVUZ"//prototyping yapmış olduk sonradan değer girdik
 console.log(customer.lastName)
 
 let user1=new User(2,"Murat","Kara","Ankara")
+let user2=new User(1,"Baran","Kökçekli","Adana")
+userService.add(user1)
+userService.add(user2)
 
-userService.add(user1.firstName)
+console.log(userService.list())
+console.log(userService.getById(2))
--- kodlama-io/week3/lesson/javascript-starter-kit/src/services/userService.js
+++ kodlama-io/week3/lesson/javascript-starter-kit/src/services/userService.js
@@ -1,26 +1,28 @@
 export default class UserService {
 
-    constructor() {
+    constructor(loggerService) {
         this.users = []
+        this.loggerService=loggerService
     }
 
     add(user) {
 
         //console.log("Kullanıcı eklendi ! : "+user)
-
         this.users.push(user)
+        //this.loggerService.log(user)
+
     }
 
     list() {
 
         //console.log("Kullanıcılar listelendi !")
         return this.users
+
     }
 
     getById(id) {
 
         //console.log("Kullanıcı detayı getirildi !")
-
         return this.users.find(u=>u.id===id)
 
     }
