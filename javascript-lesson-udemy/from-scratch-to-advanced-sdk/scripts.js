77a6c36a327e12ffd9c00f6f562fc0a8c445bb6f
Coders club <1yavuzahmet@gmail.com>
2023-08-01 21:29:18 +0300

    javascript base lesson started

---
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/scripts.js
@@ -0,0 +1,40 @@
+var maasAhmet=8000
+var zam=0.35
+
+console.log("merhaba");
+// 5000+(2*5)
+console.log(5000+(2*5))
+
+console.log(maasAhmet+(maasAhmet*zam))
+
+var urunAdi="IPhone 13";
+var urunFiyat=15000;
+var urunAdi11="IPhone 11";
+var urunFiyat11="7000";
+
+console.log(typeof urunAdi)
+console.log(typeof urunFiyat)
+console.log(Number(urunFiyat11))//stringden number a çevirdik
+console.log(urunFiyat.toString)//numberdan string e çevirir
+
+console.log(Number(urunFiyat11)+urunFiyat)
+
+///LET USING
+
+let sayi1=10;
+let sayi2=21;
+
+console.log(sayi1.toString()+sayi2.toString())//yan yana yazar çünkü string toplama işlemi yapar.
+
+let isim="Ali";
+let soyad="Kara";
+
+console.log(isim+" "+soyad);
+
+let sinavNotu=40;
+
+let basariliMi=(sinavNotu>=50);
+
+console.log(basariliMi);
+
+console.log(typeof(basariliMi));
