1f35774fa46c9231c211e956dea7f29fa6eeade4
Coders club <1yavuzahmet@gmail.com>
2023-08-08 14:38:17 +0300

    javascript arrays homeworks

---
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/10-array-homeworks.js
@@ -0,0 +1,27 @@
+let fruits=["apple","banana","pear","grape"];
+
+console.log(fruits.length);
+
+console.log(fruits[0]);
+
+console.log(fruits[fruits.length-1]);
+//or
+console.log(fruits.reverse());
+
+console.log(fruits[0]);
+
+console.log(fruits.includes("apple"));
+console.log(fruits.indexOf("apple"));
+
+console.log(fruits.push("cherry"));
+
+console.log(fruits);
+
+// fruits.pop();
+// fruits.pop();
+//or
+fruits.splice(fruits.length-2);
+console.log(fruits);
+
+
+
