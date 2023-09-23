cc88bc29261f914e9d03fcaa235d68f9e0b5633e
Coders club <1yavuzahmet@gmail.com>
2023-09-23 14:41:31 +0300

    js dom events

---
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/objects/functions.js
@@ -0,0 +1,13 @@
+/*function hello(birthOfDate) {
+  console.log(new Date().getFullYear() - birthOfDate);
+}
+
+hello(1987);
+*/
+function sendAndWrite(word,number) {
+  for (let i = 0; i < number; i++) {
+    console.log(word);
+  }
+}
+
+sendAndWrite("my name is hello", 5);
