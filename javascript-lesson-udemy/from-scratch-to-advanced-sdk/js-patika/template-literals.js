86ff18ec20b74a1060e203c25f2485c057c50841
Coders club <1yavuzahmet@gmail.com>
2023-09-26 21:53:19 +0300

    string-data

--- javascript-lesson-udemy/from-scratch-to-advanced-sdk/js-patika/template-literals.js
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/js-patika/template-literals.js
@@ -7,3 +7,15 @@ console.log("Benim adim "+ad+" "+ soyad);
 //Yeni kullanım
 console.log(`Benim adim ${ad} ${soyad}`);
 
+let email = "kodluyoruz@kodluyoruz.org"
+let firstName = "JavaScript"
+let lastName = "BOOTCAMP"
+
+console.log(email.length);
+console.log(firstName[0]);
+console.log(firstName.charAt(0));
+console.log(email.search("@"));//not found -1, found 
+console.log(email.slice(1,5))
+console.log(email.slice(email.search("@")+1)) //kodluyoruz.org
+console.log(email.slice(email.search("@")))//@kodluyoruz.org
+console.log(email.replace("@kodluyoruz.org", "@yandex.com"))
