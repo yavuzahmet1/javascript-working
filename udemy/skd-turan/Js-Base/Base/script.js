2755e07913b56944c7335414f628b1754cee1248
Coders club <1yavuzahmet@gmail.com>
2024-05-01 23:44:00 +0300

    main

--- udemy/skd-turan/Js-Base/Base/script.js
+++ udemy/skd-turan/Js-Base/Base/script.js
@@ -1,17 +1,15 @@
-let result=1;
-for(let i=1;i<10;i++){
-
-    console.log(`${i} : ${result*=i}`);
+let result = 1;
+for (let i = 1; i < 10; i++) {
+  console.log(`${i} : ${(result *= i)}`);
 }
 
-console.log("-------")
-let numbers=[1,4,5,6,7,8,9];
+console.log("-------");
+let numbers = [1, 4, 5, 6, 7, 8, 9];
 
-for(let numer in numbers ){
-console.log(numer);//index numaraları yazdırılır
-
+for (let numer in numbers) {
+  console.log(numer); //index numaraları yazdırılır
 }
 
-for(let num of numbers){
-    console.log(num)//numbers listesinin içindekiler yazdırılır.
+for (let num of numbers) {
+  console.log(num); //numbers listesinin içindekiler yazdırılır.
 }
