8d4a76ccd643e0e3da2b4fcae3523e8080b5f77a
Coders club <1yavuzahmet@gmail.com>
2024-05-12 12:27:49 +0300

    web sites

---
+++ udemy/skd-turan/Js-Base/SLIDER/script.js
@@ -0,0 +1,23 @@
+var models = [
+  {
+    name: "BMW",
+    image: "img/bmw.jpg",
+    link: "http://www.arabalar.com.tr",
+  },
+  {
+    name: "Mazda",
+    image: "img/mazda.jpg",
+    link: "http://www.arabalar.com.tr",
+  },
+  {
+    name: "Skoda",
+    image: "img/skoda.jpg",
+    link: "http://www.arabalar.com.tr",
+  },
+  {
+    name: "Volvo",
+    image: "img/volvo.jpg",
+    link: "http://www.arabalar.com.tr",
+  },
+];
+document.querySelector(".card-img-top").setAttribute("src", models[3].image);
