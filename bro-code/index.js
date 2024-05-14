654d10e08f17c7f80a9f7e65b80b3c971f109d77
Coders club <1yavuzahmet@gmail.com>
2024-05-15 00:13:12 +0300

    Bro code variables

--- bro-code/index.js
+++ bro-code/index.js
@@ -1,3 +1,6 @@
 console.log(`Hello`);
 
-window.alert(`This an alert!`)
+//window.alert(`This an alert!`)
+
+document.getElementById("myH2").textContent="Population";
+document.getElementById("myP").textContent="I like Pizza";
