331fabd06e839bf12c710bcb788d5b879795374e
Coders club <1yavuzahmet@gmail.com>
2024-05-15 23:11:44 +0300

    inpubox controls

--- bro-code/index.js
+++ bro-code/index.js
@@ -1,6 +1,17 @@
-console.log(`Hello`);
+/*console.log(`Hello`);
 
 //window.alert(`This an alert!`)
 
 document.getElementById("myH2").textContent="Population";
 document.getElementById("myP").textContent="I like Pizza";
+
+let students=30;
+//students=students**3
+students=students%4
+console.log(students)*/
+
+let username;
+
+username=window.prompt("What's your username?");
+
+console.log(username);
---
+++ bro-code/website/inpubox/input.js
@@ -0,0 +1,6 @@
+let username;
+
+document.getElementById("mySubmit").onclick=function(){
+    username=document.getElementById("myText").value;
+    document.getElementById("myH1").textContent=`Hello ${username}`;
+}
