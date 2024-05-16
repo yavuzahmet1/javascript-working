d2e907b43abbf14459b5ef6b9ea84326e8282d03
Coders club <1yavuzahmet@gmail.com>
2024-05-16 22:33:52 +0300

    Math object

---
+++ bro-code/math-object/index.js
@@ -0,0 +1,13 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 16;
+let y = 2;
+let z;
+
+//z=Math.pow(x,y);//256
+//z=Math.sqrt(x)//4
+//z=Math.sin(x)
+z = Math.round(x);
+
+console.log(z);
