f6f46757a2c5b51e01efb415059e4de0a3efe8e3
Coders club <1yavuzahmet@gmail.com>
2024-05-19 00:00:57 +0300

    payment checked

---
+++ bro-code/javascript/checked-property/index.js
@@ -0,0 +1,31 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterBtn = document.getElementById("masterBtn");
+const troyBtn = document.getElementById("troyBtn");
+const americanBtn = document.getElementById("americanBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function () {
+  if (myCheckBox.checked) {
+    subResult.textContent = "You are subcribed!";
+  } else {
+    subResult.textContent = "You are not subcribed!";
+  }
+
+  if (visaBtn.checked) {
+    paymentResult.textContent = "You are paying with Visa";
+  } else if (masterBtn.checked) {
+    paymentResult.textContent = "You are paying with Master Card";
+  } else if (troyBtn.checked) {
+    paymentResult.textContent = "You are paying with Troy";
+  } else if (americanBtn.checked) {
+    paymentResult.textContent = "You are paying with American Express";
+  } else if (payPalBtn.checked) {
+    paymentResult.textContent = "You are paying with Pay Pal";
+  }else{
+    paymentResult.textContent="You must payment checked"
+  }
+};
