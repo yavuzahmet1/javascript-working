fa4d83ff96971d2ab9556e1feaff9887dd165102
Coders club <1yavuzahmet@gmail.com>
2024-05-19 22:57:40 +0300

    rest-operators

---
+++ bro-code/javascript/rest-operators/index.js
@@ -0,0 +1,10 @@
+function fridge(...foods) {
+    console.log(foods)
+}
+
+const foods1="pizza";
+const foods2="sucuk";
+const foods3="cholate";
+const foods4="egg";
+
+fridge(foods1,foods2,foods3,foods4);
