3d4b3e5b1db397ea092c557baf768ee6e2f77216
Coders club <1yavuzahmet@gmail.com>
2024-05-22 00:08:24 +0300

    host

--- bro-code/javascript/dice-roller/index.js
+++ bro-code/javascript/dice-roller/index.js
@@ -1,3 +1,5 @@
+console.log(location.hostname)
+console.log(location.host)
 function rollDice() {
   const numOfDice = document.getElementById("numOfDice").value;
   const diceResult = document.getElementById("diceResult");
