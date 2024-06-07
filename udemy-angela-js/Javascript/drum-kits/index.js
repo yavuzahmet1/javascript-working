ee3b82a67d24921dc2f36b1ce324d052050d8297
Coders club <1yavuzahmet@gmail.com>
2024-06-07 21:05:14 +0300

    sound keypress play

--- udemy-angela-js/Javascript/drum-kits/index.js
+++ udemy-angela-js/Javascript/drum-kits/index.js
@@ -6,37 +6,46 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
     this.style.color = "white";
 */
     let buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case "w":
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        let tom3 = new Audio("sounds/tom-2.mp3");
-        tom3.play();
-        break;
-      case "d":
-        let tom4 = new Audio("sounds/tom-2.mp3");
-        tom4.play();
-        break;
-      case "j":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case "k":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case "l":
-        let kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      default:
-    }
+    makeSound(buttonInnerHTML);
   });
+}
+
+document.addEventListener("keypress", function (event) {
+  console.log(event);
+  makeSound(event.key);
+  // alert("Key was pressed!!");
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("sounds/tom-2.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("sounds/tom-2.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+  }
 }
