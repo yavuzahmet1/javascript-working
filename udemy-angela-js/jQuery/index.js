79ad2acf03cbaa80b8d0f804b1c1df0392cf8c43
Coders club <1yavuzahmet@gmail.com>
2024-06-08 22:07:52 +0300

    jquery

--- udemy-angela-js/jQuery/index.js
+++ udemy-angela-js/jQuery/index.js
@@ -1,1 +1,7 @@
-$("h1").css("color","red")
+$("h1").css("color","green")
+$("h1").css("font-size","5rem")
+//document.querySelector("h1");
+
+$("button").css("font-size",15)
+console.log($("button").css("font-size",12))
+$("button").text("Don't Click Me");
