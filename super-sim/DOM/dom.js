fa81f5e36e880d9073dc0c079c0c29478d64412e
Coders club <1yavuzahmet@gmail.com>
2024-06-09 11:58:30 +0300

    DOM

---
+++ super-sim/DOM/dom.js
@@ -0,0 +1,18 @@
+/*document.body.innerHTML = "Hello message";
+document.title = "Good job";
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.body.clientHeight);
+console.log(typeof document.body);
+console.log(document.body.innerHTML);
+
+document.body.innerHTML = "<button>excelent</button>";
+*/
+let a = document.querySelector("button").innerHTML;
+console.log(a);
+console.log(document.querySelector("button"));
+
+const b = document.querySelector(".js-button");
+console.log(b);
+console.log(document.querySelector(".js-button"));
