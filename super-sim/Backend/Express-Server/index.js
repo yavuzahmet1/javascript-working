fb865bb005ce76a5e8bfc4b3e4bba1a12655f0fa
Coders club <1yavuzahmet@gmail.com>
2024-06-17 10:54:40 +0300

    HTTP Requests

--- super-sim/Backend/Express-Server/index.js
+++ super-sim/Backend/Express-Server/index.js
@@ -1,6 +1,16 @@
 import express from "express";
 const app = express();
-const port=3000;
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, world!</h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1>");
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>Telephone : 111112233</h1>");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
