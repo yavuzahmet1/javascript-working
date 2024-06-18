e479d51ca6dcd07892f48825df7a215e0b740581
Coders club <1yavuzahmet@gmail.com>
2024-06-18 12:24:48 +0300

    validate code refactor

--- javascript-lesson-udemy/from-scratch-to-advanced-sdk/project/form-validator/script.js
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/project/form-validator/script.js
@@ -5,26 +5,57 @@ const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const repassword = document.getElementById("repassword");
 
+function error(input, message) {
+  input.className = "form-control is-invalid";
+  const div = input.nextElementSibling;
+  div.innerText = message;
+  div.className = "invalid-feedback";
+}
+
+function succes(input) {
+  input.className = "form-control is-valid";
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (emailRegex.test(email.value)) {
+    succes(email);
+  } else {
+    error(email, "The email you entered does not comply with the format");
+  }
+
+  //return emailRegex.test(String(email).toLowerCase());
+}
+function checkRequired(inputs) {
+  inputs.forEach(function (input) {
+    if (input.value === "") {
+      error(input, `${input.id} is required`);
+    } else {
+      succes(input); //dizi şeklinde aldık alanları tek tek dolaştık
+    }
+  });
+  /*
+  console.log(input.value);
+  if (inputs.value === "") {
+    error(inputs, `${inputs.id} is required`);
+  } else {
+    succes(inputs);
+  }*/
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  checkRequired([username, email, phone, password, repassword]);
+  validateEmail(email);
+  /*checkRequired(username);
+  checkRequired(email);
+  checkRequired(phone);
+  checkRequired(password);
+  checkRequired(repassword);*/
+
   //console.log(username);
-
-  function error(input, message) {
-    input.className = "form-control is-invalid";
-    const div = input.nextElementSibling;
-    div.innerText = message;
-    div.className = "invalid-feedback";
-  }
-
-  function succes(input) {
-    input.className = "form-control is-valid";
-  }
-
-  function validateEmail(email) {
-    const emailRegex = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailRegex.test(String(email).toLowerCase());
-  }
-
+  /*
   if (username.value === "") {
     //username.className='form-control error'; veya
     error(username, "username is required"); //boostrap olduğundan bu şekilde de kullanılabilir.
@@ -36,7 +67,6 @@ form.addEventListener("submit", function (e) {
     error(email, "email is required");
   } else if (!validateEmail(email.value)) {
     error(email, "The email you entered does not comply with the format");
-    console.log("çalıştı")
   } else {
     succes(email);
   }
@@ -54,5 +84,5 @@ form.addEventListener("submit", function (e) {
     error(repassword, "repassword is required");
   } else {
     succes(repassword);
-  }
+  }*/
 });
