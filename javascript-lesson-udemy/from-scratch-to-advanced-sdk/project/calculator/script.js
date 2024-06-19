ca658390901a8ede97fa54788ac0da6f97085dd1
Coders club <1yavuzahmet@gmail.com>
2024-06-19 13:01:12 +0300

    calculator app completed

--- javascript-lesson-udemy/from-scratch-to-advanced-sdk/project/calculator/script.js
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/project/calculator/script.js
@@ -2,6 +2,9 @@ const display = document.querySelector(".calculator-input");
 const keys = document.querySelector(".calculator-keys");
 
 let displayValue = "0";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 updateDisplay();
 
@@ -11,33 +14,67 @@ function updateDisplay() {
 
 keys.addEventListener("click", function (e) {
   const element = e.target;
+  const value = element.value;
+
   if (!element.matches("button")) return;
 
-  if (element.classList.contains("operator")) {
-    console.log("operator", element.value);
-    return;
+  switch (value) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+      handleOperator(value);
+      break;
+    case ".":
+      inputDecimal();
+      break;
+    case "clear":
+      clear();
+      break;
+    default:
+      inputNumber(value);
   }
+  updateDisplay();
+
+  /*if (element.classList.contains("operator")) {
+    handleOperator(element.value);
+    updateDisplay();
+    return;
+  }*/
   if (element.classList.contains("decimal")) {
-    console.log("decimal", element.value);
     inputDecimal(element.value);
     updateDisplay();
     return;
   }
   if (element.classList.contains("clear")) {
-    console.log("clear", element.value);
     clear();
     updateDisplay();
     return;
   }
-
-  // console.log(element.value);
 
   inputNumber(element.value);
   updateDisplay();
 });
 
 function inputNumber(num) {
-  displayValue = displayValue === "0" ? num : displayValue + num;
+  if (waitingForSecondValue) {
+    displayValue = num;
+    waitingForSecondValue = false;
+  } else {
+    displayValue = displayValue === "0" ? num : displayValue + num;
+  }
+
+  console.log(
+    "displayValue : ",
+    displayValue,
+    " firstValue : ",
+    firstValue,
+    " operator : ",
+    operator,
+    " waitingForSecondValue : ",
+    waitingForSecondValue
+  );
 }
 
 function inputDecimal() {
@@ -46,4 +83,45 @@ function inputDecimal() {
 
 function clear() {
   displayValue = "0";
+}
+
+function handleOperator(nextOperator) {
+  const value = parseFloat(displayValue);
+
+  if (operator && waitingForSecondValue) {
+    operator = nextOperator;
+    return;
+  }
+  if (firstValue == null) {
+    firstValue = value;
+  } else if (operator) {
+    const result = calculate(firstValue, value, operator);
+    displayValue = `${parseFloat(result.toFixed(7))}`;
+    firstValue = result;
+  }
+  waitingForSecondValue = true;
+  operator = nextOperator;
+  console.log(
+    "displayValue : ",
+    displayValue,
+    " firstValue : ",
+    firstValue,
+    " operator : ",
+    operator,
+    " waitingForSecondValue : ",
+    waitingForSecondValue
+  );
+}
+
+function calculate(first, second, operator) {
+  if (operator === "+") {
+    return first + second;
+  } else if (operator === "-") {
+    return first - second;
+  } else if (operator === "*") {
+    return first * second;
+  } else if (operator === "/") {
+    return parseFloat(first / second);
+  }
+  return second;
 }
