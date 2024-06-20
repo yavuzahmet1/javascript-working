5ec4f76d693e2bc44bfd49a9ed1b7c27253bc773
Coders club <1yavuzahmet@gmail.com>
2024-06-20 22:36:19 +0300

    quiz temlate completed

--- javascript-lesson-udemy/from-scratch-to-advanced-sdk/OOP/quiz-project/script.js
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/OOP/quiz-project/script.js
@@ -1,40 +1,55 @@
-function QuestionFunction(questionText, questionOption, correctAnswer) {
-  this.questionText = questionText;
-  this.questionOption = questionOption;
-  this.correctAnswer = correctAnswer;
+// OOP: Nesne Tabanlı Programlama
 
-  console.log(this);
+function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
+  this.soruMetni = soruMetni;
+  this.cevapSecenekleri = cevapSecenekleri;
+  this.dogruCevap = dogruCevap;
 }
 
-let questions = [
-  new QuestionFunction(
-    "JavaScript'deki yorum satirlari, _______ tarafindan yok sayilir.",
-    {
-      a: "bilgisayar ",
-      b: "işletim sistemi",
-      c: "derleyici",
-      d: "tarayici",
-    },
-    "d"
+Soru.prototype.cevabiKontrolEt = function (cevap) {
+  return cevap === this.dogruCevap;
+};
+
+let sorular = [
+  new Soru(
+    "1-Hangisi javascript paket yönetim uygulasıdır?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
   ),
-  new QuestionFunction(
-    "var str = JavaScript dilini seviyorum.; var sub = str.substr( 4, 6 );alert (sub);",
-    {
-      a: "Scr",
-      b: "Script",
-      c: "aSc",
-      d: "aScrp",
-    },
-    "b"
+  new Soru(
+    "2-Hangisi javascript paket yönetim uygulasıdır?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
   ),
-  new QuestionFunction(
-    "Which tag is correct for JavaScript?",
-    {
-      a: "<?……?> ",
-      b: "<JavaScript…...>…..</JavaScript>",
-      c: "<script Language=”JavaScript”>……</JavaScript>",
-      d: "<script type = “text/JavaScript”>……</script>",
-    },
-    "d"
+  new Soru(
+    "3-Hangisi javascript paket yönetim uygulasıdır?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
+  ),
+  new Soru(
+    "4-Hangisi javascript paket yönetim uygulasıdır?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
   ),
 ];
+
+function Quiz(sorular) {
+  this.sorular = sorular;
+  this.soruIndex = 0;
+}
+
+Quiz.prototype.soruGetir = function () {
+  return this.sorular[this.soruIndex];
+};
+
+const quiz = new Quiz(sorular);
+
+document.querySelector(".btn_start").addEventListener("click", function () {
+  if (quiz.sorular.length != quiz.soruIndex) {
+    document.querySelector(".quiz_box").classList.add("active");
+    console.log(quiz.soruGetir());
+    quiz.soruIndex += 1;
+  } else {
+    console.log("quiz bitti");
+  }
+});
