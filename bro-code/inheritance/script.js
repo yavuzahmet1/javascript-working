36917a51806646d04576e403879722a40072b1a9
Coders club <1yavuzahmet@gmail.com>
2024-06-23 21:13:55 +0300

    inheritance

---
+++ bro-code/inheritance/script.js
@@ -0,0 +1,25 @@
+class Animal {
+  alive = true;
+
+  eat() {
+    console.log(`This ${this.name} is eating`);
+  }
+  sleep() {
+    console.log(`This ${this.name} is eating`);
+  }
+}
+
+class Rabbit extends Animal {
+  name = "Rabbit";
+}
+
+class Fish extends Animal {
+  name = "Fish";
+}
+class Hawk extends Animal {
+  name = "Hawk";
+}
+
+const rabbit = new Rabbit();
+rabbit.alive = false;
+console.log(rabbit.alive);
