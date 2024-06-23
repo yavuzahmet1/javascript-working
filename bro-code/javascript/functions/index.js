6cf28b01b28325aa84994d43cea9c456510a04f1
Coders club <1yavuzahmet@gmail.com>
2024-06-23 18:15:19 +0300

    arrow function

--- bro-code/javascript/functions/index.js
+++ bro-code/javascript/functions/index.js
@@ -1,5 +1,15 @@
 function happyBirthDay(params) {
-    console.log(`Happy Birth Day ${params}`)
-    
+  console.log(`Happy Birth Day ${params}`);
 }
-happyBirthDay("Ahmet")
+happyBirthDay("Ahmet");
+
+const numbers = [1, 25, 36, 91];
+const squares = numbers.map(function (element) {
+  return Math.pow(element, 2);
+});
+
+console.log(squares);
+
+const hello = () => console.log("hello");
+
+hello();
