fc1866e089eac3073b8f6791dfdc9f64870ce60d
Coders club <1yavuzahmet@gmail.com>
2024-06-25 23:10:36 +0300

    course app refactor

--- javascript-lesson-udemy/from-scratch-to-advanced-sdk/course-app/appES5.js
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/course-app/appES5.js
@@ -1,18 +1,17 @@
 // Course constructor
-function Course(title,instructor ,image){
-    this.title = title;
-    this.instructor = instructor;
-    this.image = image;
+function Course(title, instructor, image) {
+  this.title = title;
+  this.instructor = instructor;
+  this.image = image;
 }
 
 // UI constructor
-function UI(){
-}
+function UI() {}
 
-UI.prototype.addCourseToList = function(course){
-    const list = document.getElementById('course-list');
+UI.prototype.addCourseToList = function (course) {
+  const list = document.getElementById("course-list");
 
-    var html = `
+  var html = `
          <tr>
             <td><img src="img/${course.image}"/></td>
             <td>${course.title}</td>
@@ -21,68 +20,65 @@ UI.prototype.addCourseToList = function(course){
          </tr>    
     `;
 
-    list.innerHTML += html;
-}
+  list.innerHTML += html;
+};
 
-UI.prototype.clearControls = function(){
-    const title = document.getElementById('title').value="";
-    const instructor = document.getElementById('instructor').value="";
-    const image = document.getElementById('image').value="";
-}
+UI.prototype.clearControls = function () {
+  const title = (document.getElementById("title").value = "");
+  const instructor = (document.getElementById("instructor").value = "");
+  const image = (document.getElementById("image").value = "");
+};
 
-UI.prototype.deleteCourse = function(element){
-    if(element.classList.contains('delete')){
-        element.parentElement.parentElement.remove();
-    }
-}
+UI.prototype.deleteCourse = function (element) {
+  if (element.classList.contains("delete")) {
+    element.parentElement.parentElement.remove();
+  }
+};
 
-UI.prototype.showAlert = function(message,className){
-    
-    var alert = `
+UI.prototype.showAlert = function (message, className) {
+  var alert = `
         <div class="alert alert-${className}">
             ${message}
         </div>    
     `;
 
-    const row = document.querySelector('.row');
-    // beforeBegin , afterBegin , beforeEnd , afterEnd
-    row.insertAdjacentHTML('beforeBegin',alert);
+  const row = document.querySelector(".row");
+  // beforeBegin , afterBegin , beforeEnd , afterEnd
+  row.insertAdjacentHTML("beforeBegin", alert);
 
-    setTimeout(()=>{
-        document.querySelector('.alert').remove();
-    },3000);
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
 
-}
+document.getElementById("new-course").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value;
+  const instructor = document.getElementById("instructor").value;
+  const image = document.getElementById("image").value;
 
-document.getElementById('new-course').addEventListener('submit',function(e){
+  // create course object
+  const course = new Course(title, instructor, image);
 
-    const title = document.getElementById('title').value;
-    const instructor = document.getElementById('instructor').value;
-    const image = document.getElementById('image').value;
+  // create UI
+  const ui = new UI();
 
-    // create course object
-    const course = new Course(title,instructor,image);
+  if (title === "" || instructor === "" || image === "") {
+    ui.showAlert("Please complete the form", "warning");
+  } else {
+    // add course to list
+    ui.addCourseToList(course);
 
-    // create UI
-    const ui = new UI();
+    // clear controls
+    ui.clearControls();
 
-    if(title==='' || instructor ==='' || image === ''){
-        ui.showAlert('Please complete the form','warning');
-    }else{
-        // add course to list
-        ui.addCourseToList(course); 
+    ui.showAlert("the course has been added", "success");
+  }
 
-        // clear controls
-        ui.clearControls();
-
-        ui.showAlert('the course has been added','success');
-    }
-
-    e.preventDefault();
+  e.preventDefault();
 });
 
-document.getElementById('course-list').addEventListener('click',function(e){
-    const ui = new UI();
-    ui.deleteCourse(e.target);
-    ui.showAlert('the course has been deleted','danger');
+document.getElementById("course-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteCourse(e.target);
+  ui.showAlert("the course has been deleted", "danger");
 });
--- javascript-lesson-udemy/from-scratch-to-advanced-sdk/course-app/appES6.js
+++ javascript-lesson-udemy/from-scratch-to-advanced-sdk/course-app/appES6.js
@@ -1,16 +1,16 @@
 class Course {
-    constructor(title, instructor, image){
-        this.title = title;
-        this.instructor = instructor;
-        this.image = image;
-    }
+  constructor(title, instructor, image) {
+    this.title = title;
+    this.instructor = instructor;
+    this.image = image;
+  }
 }
 
 class UI {
-    addCourseToList(course) {
-        const list = document.getElementById('course-list');
+  addCourseToList(course) {
+    const list = document.getElementById("course-list");
 
-        var html = `
+    var html = `
          <tr>
             <td><img src="img/${course.image}"/></td>
             <td>${course.title}</td>
@@ -19,69 +19,68 @@ class UI {
          </tr>    
     `;
 
-     list.innerHTML += html;
-    }
+    list.innerHTML += html;
+  }
 
-    clearControls(){
-        const title = document.getElementById('title').value="";
-        const instructor = document.getElementById('instructor').value="";
-        const image = document.getElementById('image').value="";
-    }
+  clearControls() {
+    const title = (document.getElementById("title").value = "");
+    const instructor = (document.getElementById("instructor").value = "");
+    const image = (document.getElementById("image").value = "");
+  }
 
-    deleteCourse(element){
-        if(element.classList.contains('delete')){
-            element.parentElement.parentElement.remove();
-        }
+  deleteCourse(element) {
+    if (element.classList.contains("delete")) {
+      element.parentElement.parentElement.remove();
     }
+  }
 
-    showAlert(message, className){
-        var alert = `
+  showAlert(message, className) {
+    var alert = `
          <div class="alert alert-${className}">
             ${message}
          </div>    
         `;
 
-        const row = document.querySelector('.row');
-        // beforeBegin , afterBegin , beforeEnd , afterEnd
-        row.insertAdjacentHTML('beforeBegin',alert);
+    const row = document.querySelector(".row");
+    // beforeBegin , afterBegin , beforeEnd , afterEnd
+    row.insertAdjacentHTML("beforeBegin", alert);
 
-        setTimeout(()=>{
-            document.querySelector('.alert').remove();
-        },3000);
-    }
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
 }
 
-document.getElementById('new-course').addEventListener('submit',function(e){
+document.getElementById("new-course").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value;
+  const instructor = document.getElementById("instructor").value;
+  const image = document.getElementById("image").value;
 
-    const title = document.getElementById('title').value;
-    const instructor = document.getElementById('instructor').value;
-    const image = document.getElementById('image').value;
+  // create course object
+  const course = new Course(title, instructor, image);
 
-    // create course object
-    const course = new Course(title,instructor,image);
+  // create UI
+  const ui = new UI();
 
-    // create UI
-    const ui = new UI();
+  console.log(ui);
 
-    console.log(ui);
+  if (title === "" || instructor === "" || image === "") {
+    ui.showAlert("Please complete the form", "warning");
+  } else {
+    // add course to list
+    ui.addCourseToList(course);
 
-    if(title==='' || instructor ==='' || image === ''){
-        ui.showAlert('Please complete the form','warning');
-    }else{
-        // add course to list
-        ui.addCourseToList(course); 
+    // clear controls
+    ui.clearControls();
 
-        // clear controls
-        ui.clearControls();
+    ui.showAlert("the course has been added", "success");
+  }
 
-        ui.showAlert('the course has been added','success');
-    }
-
-    e.preventDefault();
+  e.preventDefault();
 });
 
-document.getElementById('course-list').addEventListener('click',function(e){
-    const ui = new UI();
-    ui.deleteCourse(e.target);
-    ui.showAlert('the course has been deleted','danger');
+document.getElementById("course-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteCourse(e.target);
+  ui.showAlert("the course has been deleted", "danger");
 });
