822ad6ee244a4bcdd73111c5b55a271096708b25
1yavuz <1yavuzahmet@gmail.com>
2024-09-16 20:35:11 +0300

    prototype

--- kodlama-io/week1/lesson/javascript-starter-kit/src/app.js
+++ kodlama-io/week1/lesson/javascript-starter-kit/src/app.js
@@ -1,25 +1,30 @@
-console.log("merhaba Kodlama.io")
+console.log("merhaba Kodlama.io");
 
-let dolarDun=9.20
-let dolarBugun=9.44
+let dolarDun = 9.2;
+let dolarBugun = 9.44;
 {
-    let dolarDun=8.99
-    //var dolarDun=8.99 olsaydı global değişken olacğından hata alacaktık
-    
+  let dolarDun = 8.99;
+  //var dolarDun=8.99 olsaydı global değişken olacğından hata alacaktık
 }
-console.log(dolarDun)//süslü parantez içindekini çalıştırmaz çünkü orası ayrı blok olarak tanımlanır
-console.log(dolarBugun1)
-let dolarBugun1=9
+console.log(dolarDun); //süslü parantez içindekini çalıştırmaz çünkü orası ayrı blok olarak tanımlanır
+console.log(dolarBugun1);
+let dolarBugun1 = 9;
 
-const euroDun=11
+const euroDun = 11;
 
-console.log(euroDun)
+console.log(euroDun);
 
 //array
-let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi",["a","b","c"]]
+let konutKredileri = [
+  "Konut Kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+  ["a", "b", "c"],
+];
 
-console.log(konutKredileri)
+console.log(konutKredileri);
+console.log(konutKredileri.toString());
 
-for (let i=0;i<konutKredileri.length;i++){
-    console.log(konutKredileri[i]);
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log(konutKredileri[i]);
 }
