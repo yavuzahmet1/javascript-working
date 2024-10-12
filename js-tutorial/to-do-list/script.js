9ab8fd8a50b5734696a8065ed820b7d0ec53ab14
1yavuz <1yavuzahmet@gmail.com>
2024-10-12 21:38:56 +0300

    todolist completed

--- js-tutorial/to-do-list/script.js
+++ js-tutorial/to-do-list/script.js
@@ -1,12 +1,29 @@
-const add = document.getElementById("add-btn");
-const clear = document.getElementById("clear-btn");
-const inputBox = document.getElementById("input-box");
-const toDoContainer = document.getElementById("to-do-container")
+const addBtn = document.getElementById("add-btn");
+const clearBtn = document.getElementById("clear-btn");
+const inputBox = document.querySelector("#input-box");
+const container = document.querySelector("#to-do-container");
 
 
-add.addEventListener("click", () => {
-    let paragraph = document.createElement("p");
-    toDoContainer.appendChild(paragraph);
-    paragraph.textContent = inputBox.value
-    inputBox.value = ""
+addBtn.addEventListener("click", () => {
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+    container.appendChild(paragraph);
+    paragraph.innerHTML = inputBox.value;
+    inputBox.value = "";
+    inputBox.focus();
+
+    paragraph.addEventListener("click", () => {
+        paragraph.style.textDecoration = "line-through";
+        paragraph.style.color = "grey"
+        console.log(event.target)
+    })
+    paragraph.addEventListener("dblclick", () => {
+        paragraph.remove();
+        console.log(event.target)
+    })
+
+    clearBtn.addEventListener("click", () => {
+        paragraph.style.color = "red"
+        console.log(event.target)
+    })
 })
